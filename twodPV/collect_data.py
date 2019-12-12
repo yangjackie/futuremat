bb16517f4b8e0270d3ac3d45fb711fca1ec0586c
yangjackie <jy2c13@soton.ac.uk>
2019-12-12 11:37:12 +1100

    database working

--- twodPV/collect_data.py
+++ twodPV/collect_data.py
@@ -1,41 +1,85 @@
+"""
+This module contains methods to collect results from the calculation folder
+and stored them in a proper object-relational-database. The io functionaities
+from the ase packages will be used because it already has a good interface to handle
+all the database operations. In this case, the data structures from ASE will
+also be used rather than our internal ones. This also allows our data to be
+more easily accessible by other users using the existing ase functionalities.
+"""
+
 import argparse
 import os
 
-from ase.db import *
-
 from core.calculators.vasp import Vasp
-from core.dao.vasp import VaspReader
+from ase.io.vasp import *
+from ase.db import connect
 
-def element_energy():
-    print ("========== Collecting energies for constituting elements ===========")
+reference_atomic_energies={}
+
+def _atom_dict(atoms):
+    """
+    Get a dictionary of number of each element in the chemical structure.
+    """
+    unique = list(set(atoms.get_chemical_symbols()))
+    return {u:atoms.get_chemical_symbols().count(u) for u in unique}
+
+def populate_db(db,atoms,kvp,data):
+    row=None
+    try:
+        row = db.get(selection=[('uid','=',kvp['uid'])])
+        #There is already something matching this row, we will update the key-value pairs and data before commit
+        kvp.update(row.key_value_pairs)
+        if data is not None:
+            data.update(row.data)
+        db.write(atoms,data=data,id=row.id,**kvp)
+    except KeyError:
+        db.write(atoms,data=data,**kvp)
+
+def element_energy(db):
+    print ("========== Collecting reference energies for constituting elements ===========")
     cwd = os.getcwd()
     element_directory = cwd+'/elements/'
     for dir in [o for o in os.listdir(element_directory) if os.path.isdir(os.path.join(element_directory,o))]:
-        element = dir
+        kvp={}
+        data={}
+
         dir = os.path.join(element_directory,dir)
+        uid = 'element_'+dir.split('/')[-1]
+
         os.chdir(dir)
         calculator = Vasp()
         calculator.check_convergence()
         if calculator.completed:
-            tot_e = VaspReader(input_location='./OSZICAR').get_free_energies_from_oszicar()[-1]
-            crystal = VaspReader(input_location='./POSCAR').read_POSCAR()
-            elemental_energy = tot_e / crystal.all_atoms_count_dictionaries()[element]
-            print(element,elemental_energy)
+            atoms = [i for i in read_vasp_xml(index=-1)][-1] #just to be explicit that we want the very last one
+            e = list(_atom_dict(atoms).keys())[-1]
+            reference_atomic_energies[e] = atoms.get_calculator().get_potential_energy()/_atom_dict(atoms)[e]
+            kvp['uid'] = uid
+            populate_db(db,atoms,kvp,data)
         else:
             raise Exception("Vasp calculation incomplete in "+dir+". Please check!")
         os.chdir(cwd)
 
-def collect():
+def collect(db):
+
     errors = []
     steps = [element_energy]
     for step in steps:
         try:
-            step()
+            step(db)
         except Exception as x:
             error = '{}: {}'.format(x.__class__.__name__, x)
             errors.append(error)
     return errors
 
-
 if __name__=="__main__":
-    collect()
+    parser = argparse.ArgumentParser(description='Collect data for c2db.db')
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    args = parser.parse_args()
+
+    # We use absolute path because of chdir below!
+    dbname = os.path.join(os.getcwd(), '2dpv.db')
+    if not args.dry_run:
+        db = connect(dbname)
+        print('Established a sqlite3 database object '+str(db))
+
+    collect(db)
