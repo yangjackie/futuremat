6cc5c5400fb930243418a01a381f343d6bbbe621
yangjackie <jy2c13@soton.ac.uk>
2020-07-07 15:21:06 +1000

    Refector the anharmonic_score.py

--- core/external/vasp/anharmonic_score.py
+++ core/external/vasp/anharmonic_score.py
@@ -6,7 +6,10 @@
 #
 # The author has an implementation to interface wtih FIH-aim code, and here it is adopted for the VASP code.
 
+# this is a demonstration
+
 from core.dao.vasp import VaspReader
+from core.models.crystal import Crystal
 import xml.etree.cElementTree as etree
 import argparse
 import numpy as np
@@ -14,7 +17,114 @@ import phonopy
 import os
 import matplotlib.pyplot as plt
 
-if __name__=="__main__":
+
+class AnharmonicScore(object):
+
+    def __init__(self, ref_frame=None, md_fromes=None, potim=1, force_constants="force_constants.hdf5",
+                 supercell=[1, 1, 1]):
+        if isinstance(ref_frame, Crystal):
+            self.ref_frame = ref_frame
+        elif ('POSCAR' in ref_frame) or ('CONTCAR' in ref_frame):
+            print("initialising reference frame from POSCAR ")
+            self.ref_frame = VaspReader(input_location=args.ref_frame).read_POSCAR()
+
+        self.ref_coords = np.array([[a.scaled_position.x, a.scaled_position.y, a.scaled_position.z] for a in
+                                    self.ref_frame.asymmetric_unit[0].atoms])
+
+        self.md_frames = md_fromes  # require the vasprun.xml containing the MD data
+
+        self.get_all_md_forces()
+        self.get_all_md_atomic_displacements()
+
+        if isinstance(force_constants, str):
+            phonon = phonopy.load(supercell_matrix=supercell,  # WARNING - hard coded!
+                                  primitive_matrix='auto',
+                                  unitcell_filename=ref_frame,
+                                  force_constants_filename=force_constants)
+
+        new_shape = np.shape(phonon.force_constants)[0] * np.shape(phonon.force_constants)[2]
+        self.force_constant = np.reshape(phonon.force_constants, (new_shape, new_shape))
+        print("Force constants ready")
+
+        self.time_series = [t*potim for t in range(len(self.all_displacements))]
+
+    @property
+    def lattice_vectors(self):
+        """
+        :return: A numpy array representation of the lattice vector
+        """
+        _lv = self.ref_frame.lattice.lattice_vectors
+        return np.array(
+            [[_lv[0][0], _lv[0][1], _lv[0][2]], [_lv[1][0], _lv[1][1], _lv[1][2]], [_lv[2][0], _lv[2][1], _lv[2][2]]])
+
+    def get_all_md_forces(self):
+        all_forces = []
+        for event, elem in etree.iterparse(self.md_frames):
+            if elem.tag == 'varray':
+                if elem.attrib['name'] == 'forces':
+                    this_forces = []
+                    for v in elem:
+                        this_force = [float(_v) for _v in v.text.split()]
+                        this_forces.append(this_force)
+                    all_forces.append(this_forces)
+        self.all_forces = np.array(all_forces)
+        print("Atomic forces along the MD trajectory loaded")
+
+        # convert to Cartesian
+        for i in range(self.all_forces.shape[0]):
+            self.all_forces[i, :, :] = np.dot(self.all_forces[i, :, :], self.lattice_vectors)
+
+    def get_all_md_atomic_displacements(self):
+        all_positions = []
+        for event, elem in etree.iterparse(args.md_xml):
+            if elem.tag == 'varray':
+                if elem.attrib['name'] == 'positions':
+                    this_positions = []
+                    for v in elem:
+                        this_position = [float(_v) for _v in v.text.split()]
+                        this_positions.append(this_position)
+                    all_positions.append(np.array(this_positions))
+        # only need those with forces
+        all_positions = all_positions[-len(self.all_forces):]
+        all_positions = np.array(all_positions)
+        print("Atomic positions along the MD trajectory loaded, converting to displacement, taking into account PBC")
+
+        self.all_displacements = np.array(
+            [all_positions[i, :] - self.ref_coords[:] for i in range(all_positions.shape[0])])
+        # impose periodic boundary conditions, no atomic displacement in fractional coordinates should be greater than
+        # the length of each unit cell dimension
+        self.all_displacements[self.all_displacements < -0.5] += 1
+        self.all_displacements[self.all_displacements > 0.5] -= 1
+
+        # Convert to Cartesian
+        for i in range(self.all_displacements.shape[0]):
+            self.all_displacements[i, :, :] = np.dot(self.all_displacements[i, :, :], self.lattice_vectors)
+
+    @property
+    def harmonic_forces(self):
+        if (not hasattr(self,'_harmonic_forces')) or (hasattr(self,'_harmonic_forces') and self._harmonic_force is None):
+            self._harmonic_force = np.zeros(np.shape(self.all_forces))
+            for i in range(np.shape(self.all_displacements)[0]):
+                self._harmonic_force[i, :, :] = -(self.force_constant @ self.all_displacements[i, :, :].flatten()).reshape(
+                    self.all_displacements[0, :, :].shape)
+        return self._harmonic_force
+
+    @property
+    def anharmonic_forces(self):
+        if (not hasattr(self,'_anharmonic_forces')) or (hasattr(self,'_anharmonic_forces')  and self._anharmonic_forces is None):
+            self._anharmonic_forces = self.all_forces - self.harmonic_forces
+        return self._anharmonic_forces
+
+    def structure_averaged_sigma_trajectory(self):
+        self.sigma_frames = []
+        for i in range(np.shape(self.all_displacements)[0]):
+            rmse = self.anharmonic_forces[i, :, :].std()
+            std = self.all_forces[i, :, :].std()
+            sigma_frame = rmse / std
+            self.sigma_frames.append(sigma_frame)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Options for analyzing anharmonic scores from MD trajectory',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--md_xml", type=str,
@@ -23,97 +133,8 @@ if __name__=="__main__":
                         help="POSCAR for the reference frame containing the static atomic positions at 0K")
     args = parser.parse_args()
 
-    ref_frame = VaspReader(input_location=args.ref_frame).read_POSCAR()
-    all_elements = [a.label for a in ref_frame.asymmetric_unit[0].atoms]
-    ref_coords = np.array(
-        [[a.scaled_position.x, a.scaled_position.y, a.scaled_position.z] for a in ref_frame.asymmetric_unit[0].atoms])
-    print("Reference frame loaded")
-
-    _lv = ref_frame.lattice.lattice_vectors
-    lv = np.array(
-        [[_lv[0][0], _lv[0][1], _lv[0][2]], [_lv[1][0], _lv[1][1], _lv[1][2]], [_lv[2][0], _lv[2][1], _lv[2][2]]])
-
-    #Extracting all the forces from vasprun.xml
-    all_forces = []
-    for event, elem in etree.iterparse(args.md_xml):
-        if elem.tag == 'varray':
-            if elem.attrib['name'] == 'forces':
-                this_forces = []
-                for v in elem:
-                    this_force = [float(_v) for _v in v.text.split()]
-                    this_forces.append(this_force)
-                all_forces.append(this_forces)
-    all_forces = np.array(all_forces)
-    print("Atomic forces along the MD trajectory loaded")
-
-    #Extract all the atomic positions from the vasprun.xml
-    all_positions =[]
-    for event, elem in etree.iterparse(args.md_xml):
-        if elem.tag == 'varray':
-            if elem.attrib['name'] == 'positions':
-                this_positions = []
-                for v in elem:
-                    this_position = [float(_v) for _v in v.text.split()]
-                    this_positions.append(this_position)
-                all_positions.append(np.array(this_positions))
-    #only need those with forces
-    all_positions = all_positions[-len(all_forces):]
-    all_positions = np.array(all_positions)
-    print("Atomic positions along the MD trajectory loaded, converting to displacement, taking into account PBC")
-
-    all_displacements = np.array([all_positions[i, :] - ref_coords[:] for i in range(all_positions.shape[0])])
-    # impose periodic boundary conditions, no atomic displacement in fractional coordinates should be greater than
-    # the length of each unit cell dimension
-    all_displacements[all_displacements < -0.5] += 1
-    all_displacements[all_displacements > 0.5] -= 1
-
-    #now need to convert the fractional coordinates to Cartesian Coordinates
-    for i in range(all_forces.shape[0]):
-        all_forces[i, :, :] = np.dot(all_forces[i, :, :], lv)
-
-    for i in range(all_displacements.shape[0]):
-        all_displacements[i, :, :] = np.dot(all_displacements[i, :, :], lv)
-
-    ##Inspecting what the distribution of atomic forces looks like
-    #_forces = all_forces.flatten()
-    #plt.hist(_forces, bins=1000)
-    #plt.tight_layout()
-    #plt.savefig('force_stats.pdf')
-
-    print("Now retrieving the force constant matrix from PHONOPY calculations")
-
-    #cwd = os.getcwd()
-    #os.chdir('./phonon')
-
-    # After FORCE_SETS is produced from Phonopy calculation, the following needs to be run to get the force_constants file
-    #  ~/.local/bin/phonopy
-    phonon = phonopy.load(supercell_matrix=[1, 1, 1], #WARNING - hard coded!
-                          primitive_matrix='auto',
-                          unitcell_filename="POSCAR_equ",
-                          force_constants_filename="force_constants.hdf5")
-    #os.chdir(cwd)
-
-    #Now calculate the harmonic forces expected on atom displaced by delta R
-    #see https://gitlab.com/vibes-developers/vibes/-/blob/master/vibes/molecular_dynamics/utils.py
-    new_shape = np.shape(phonon.force_constants)[0]*np.shape(phonon.force_constants)[2]
-    force_constant = np.reshape(phonon.force_constants,(new_shape,new_shape))
-
-    harmonic_force = np.zeros(np.shape(all_forces))
-    anharmonic_force_component = np.zeros(np.shape(all_forces))
-
-    all_sigma=[]
-
-    for i in range(np.shape(all_displacements)[0]):
-        harmonic_force[i,:,:] = -(force_constant @ all_displacements[i,:,:].flatten()).reshape(all_displacements[0,:,:].shape)
-        anharmonic_force_component[i,:,:]= all_forces[i,:,:] - harmonic_force[i,:,:]
-        rmse = anharmonic_force_component[i,:,:].std()
-        std = all_forces[i,:,:].std()
-        sigma_frame = rmse/std
-        all_sigma.append(sigma_frame)
-
-    plt.hist2d(all_forces.flatten(),anharmonic_force_component.flatten(),bins=1000)
-    plt.xlim([-5,5])
-    plt.ylim([-5,5])
-    #plt.plot(all_sigma,'b-')
-    #plt.tight_layout()
+    scorer = AnharmonicScore(md_fromes=args.md_xml, ref_frame=args.ref_frame)
+    scorer.structure_averaged_sigma_trajectory()
+    plt.plot(scorer.time_series, scorer.sigma_frames, 'b-')
+    plt.tight_layout()
     plt.savefig('sigma.pdf')
