correct hybrid calculation procedure. save

core/external/vasp/analysis.py
@@ -10,7 +10,7 @@ from pymatgen.electronic_structure.plotter import BSPlotter
 import numpy as np
 import math
 import scipy
-from scipy.interpolate import spline
+from scipy.interpolate import interp1d
 from scipy.fftpack import fft, ifft, fftfreq, fftshift
 import glob
 
@@ -119,7 +119,6 @@ def plot_simple_smoothed_band_structure(ylim=[-1.5, 3.5], filename=None):
     else:
         BSPlotter(bs).save_plot(filename)
 
-
 def plot_total_density_of_states(xlim=None, ylim=None, filename=None):
     vasprun = Vasprun('./vasprun.xml')
     if not vasprun.is_spin:
@@ -129,19 +128,24 @@ def plot_total_density_of_states(xlim=None, ylim=None, filename=None):
 
     xnew = np.linspace(min(vasprun.tdos.energies - vasprun.tdos.efermi),
                        max(vasprun.tdos.energies - vasprun.tdos.efermi), 10 * len(vasprun.tdos.energies))
-    power_smooth = spline(vasprun.tdos.energies - vasprun.tdos.efermi, dos, xnew)
+    power_smooth = interp1d(vasprun.tdos.energies - vasprun.tdos.efermi, dos)
 
-    plt.plot(power_smooth, xnew, 'b')
+    #plt.plot(xnew, power_smooth(xnew), 'b')
+    plt.plot(vasprun.tdos.energies - vasprun.tdos.efermi, dos, 'b')
 
     if xlim is not None:
         plt.xlim(xlim)
 
     if ylim is not None:
         plt.ylim(ylim)
+    #xelse:
+        #plt.ylim([0,max(dos)+0.05*max(dos)])
 
-    plt.xlabel("Density-of-states (states/eV/cell)", fontsize=12)
-    plt.ylabel("$E-E_{F}$ (eV)", fontsize=12)
+    plt.ylabel("Density-of-states (states/eV/cell)", fontsize=12)
+    plt.xlabel("$E-E_{F}$ (eV)", fontsize=12)
     plt.tight_layout()
+
+    plt.xlim([-5,5])
 
     if filename is None:
         plt.show()
@@ -472,7 +476,7 @@ if __name__ == "__main__":
     if args.simple_band:
         plot_simple_smoothed_band_structure(ylim=[args.Emin, args.Emax], filename=args.output)
     if args.total_dos:
-        plot_total_density_of_states(ylim=[args.Emin, args.Emax], filename=args.output)
+        plot_total_density_of_states(filename=args.output)
     if args.md:
         plot_MD_energies_and_temperature_evolution(time_step=args.time_step)
     if args.displacement_traj or args.displacement_histograms:

core/external/vasp/anharmonic_score.py
@@ -24,7 +24,7 @@ from sklearn.neighbors import KernelDensity
 class AnharmonicScore(object):
 
     def __init__(self, ref_frame=None,
-                 md_fromes=None,
+                 md_frames=None,
                  potim=1,
                  force_constants="force_constants.hdf5",
                  supercell=[1, 1, 1],
@@ -44,7 +44,7 @@ class AnharmonicScore(object):
             self.atom_masks = [id for id, atom in enumerate(self.ref_frame.asymmetric_unit[0].atoms) if
                                atom.label in atoms]
 
-        self.md_frames = md_fromes  # require the vasprun.xml containing the MD data
+        self.md_frames = md_frames  # require the vasprun.xml containing the MD data
 
         self.get_dft_md_forces()
         self.get_all_md_atomic_displacements()
@@ -65,24 +65,26 @@ class AnharmonicScore(object):
         new_shape = np.shape(phonon.force_constants)[0] * np.shape(phonon.force_constants)[2]
 
         # this is WRONG, DID not put PHI_ii correctly along the diagonal!!!
-        #self.force_constant = np.reshape(phonon.force_constants, (new_shape, new_shape))
+        # self.force_constant = np.reshape(phonon.force_constants, (new_shape, new_shape))
 
-        self.force_constant = np.zeros((new_shape,new_shape))
+        self.force_constant = np.zeros((new_shape, new_shape))
 
         for i in range(np.shape(phonon.force_constants)[0]):
             for j in range(np.shape(phonon.force_constants)[1]):
                 for k in range(np.shape(phonon.force_constants)[2]):
                     for l in range(np.shape(phonon.force_constants)[3]):
-                        self.force_constant[i*3+k][j*3+l] = phonon.force_constants[i][j][k][l]
+                        #print(i * 3 + k,j * 3 + l,i,j,k,l)
+                        self.force_constant[i * 3 + k][j * 3 + l] = phonon.force_constants[i][j][k][l]
 
         print("RESHAPED PHONOPY force constant shape ", np.shape(self.force_constant))
-        #plt.matshow(self.force_constant)
-        #plt.colorbar()
-        #plt.savefig('fc.pdf')
-        #raise Exception()
         print("force constant reshape ", np.shape(self.force_constant))
         print("Force constants ready")
         self.time_series = [t * potim for t in range(len(self.all_displacements))]
+
+    def plot_fc(self):
+        plt.matshow(self.force_constant)
+        plt.colorbar()
+        plt.savefig('fc.pdf')
 
     @property
     def lattice_vectors(self):
@@ -92,7 +94,6 @@ class AnharmonicScore(object):
         _lv = self.ref_frame.lattice.lattice_vectors
         return np.array(
             [[_lv[0][0], _lv[0][1], _lv[0][2]], [_lv[1][0], _lv[1][1], _lv[1][2]], [_lv[2][0], _lv[2][1], _lv[2][2]]])
-
 
     def get_dft_md_forces(self):
         all_forces = []
@@ -182,8 +183,8 @@ class AnharmonicScore(object):
         out = np.zeros(np.shape(dft_forces))
         np.divide(dft_forces, dft_forces_std, out=out)
         if flat:
-            return out.flatten()
-        return out
+            return out.flatten(), dft_forces_std
+        return out, dft_forces_std
 
     def atom_normalized_anharmonic_forces(self, atom, flat=False):
         _mask = [id for id, a in enumerate(self.ref_frame.asymmetric_unit[0].atoms) if a.label == atom]
@@ -208,7 +209,7 @@ class AnharmonicScore(object):
             divider = make_axes_locatable(axs[id])
             cax1 = divider.append_axes("right", size="5%", pad=0.05)
 
-            X = self.atom_normalized_dft_forces(atom, flat=True)
+            X, std = self.atom_normalized_dft_forces(atom, flat=True)
             Y = self.atom_normalized_anharmonic_forces(atom, flat=True)
 
             X_pred, Y_pred = np.mgrid[-2:2:50j, -2:2:50j]
@@ -219,41 +220,59 @@ class AnharmonicScore(object):
             Z = np.reshape(kernel(positions).T, X_pred.shape)
 
             print("Making the plot")
-            a = axs[id].imshow(np.rot90(Z), cmap=plt.get_cmap('Blues'), extent=[-2, 2, -2, 2])
+            a = axs[id].imshow(np.rot90(Z), cmap=plt.get_cmap('Blues'), extent=[-1, 1, -1, 1])
 
-            axs[id].set_xlim([-2, 2])
-            axs[id].set_ylim([-2, 2])
-            axs[id].set_xlabel("$F/\\sigma(F)$", fontsize=16)
+            axs[id].plot([-1, -0.5, 0, 0.5, 1], [std for i in range(5)], 'k--')
+            axs[id].plot([-1, -0.5, 0, 0.5, 1], [-1.0 * std for i in range(5)], 'k--')
+
+            axs[id].set_xlim([-1, 1])
+            axs[id].set_ylim([-1, 1])
+            axs[id].set_xlabel("$F_{i}/\\sigma(F_{i})$", fontsize=16)
             axs[id].set_ylabel("$F^{A}_{i}/\\sigma(F_{i})$", fontsize=16)
             axs[id].set_title(str(atom), fontsize=20)
             axs[id].tick_params(axis='both', which='major', labelsize=10)
             plt.colorbar(a, cax=cax1)  # .set_label(label='probability density',size=15)
         plt.tight_layout()
-        plt.savefig('atom_joint_PDF.png')
+        plt.savefig('atom_joint_PDF.pdf')
 
-    def plot_total_joint_distribution(self):
-        X = self.dft_forces.flatten() / self.dft_forces.flatten().std()
-        Y = self.anharmonic_forces.flatten() / self.dft_forces.flatten().std()
+    def plot_total_joint_distribution(self, x='DFT', y='anh'):
+        if x == 'DFT':
+            X = self.dft_forces.flatten() / self.dft_forces.flatten().std()
+        else:
+            raise NotImplementedError()
+        if y == 'anh':
+            Y = self.anharmonic_forces.flatten() / self.dft_forces.flatten().std()
+        elif y == 'har':
+            Y = self.harmonic_forces.flatten() / self.dft_forces.flatten().std()
+        else:
+            raise NotImplementedError()
 
-        X_pred, Y_pred = np.mgrid[-2:2:50j, -2:2:50j]
+        X_pred, Y_pred = np.mgrid[-1:1:50j, -1:1:50j]
         positions = np.vstack([X_pred.ravel(), Y_pred.ravel()])
         values = np.vstack([X, Y])
         print("Gaussian KDE")
         kernel = gaussian_kde(values)
         print("Kernel done")
         Z = np.reshape(kernel(positions).T, X_pred.shape)
-        plt.imshow(np.rot90(Z), cmap=plt.get_cmap('Blues'), extent=[-2, 2, -2, 2])
-        plt.xlim([-2, 2])
-        plt.ylim([-2, 2])
+        plt.imshow(np.rot90(Z), cmap=plt.get_cmap('Blues'), extent=[-1, 1, -1, 1])
+        plt.xlim([-1, 1])
+        plt.ylim([-1, 1])
 
         plt.xlabel("$F/\\sigma(F)$", fontsize=16)
-        plt.ylabel("$F^{a}/\\sigma(F)$", fontsize=16)
+        if y == 'anh':
+            plt.ylabel("$F^{A}/\\sigma(F)$", fontsize=16)
+        elif y == 'har':
+            plt.ylabel("$F^{(2)}/\\sigma(F)$", fontsize=16)
+
+        if y == 'anh':
+            plt.plot([-1, -0.5, 0, 0.5, 1], [self.anharmonic_forces.flatten().std() for i in range(5)], 'k--')
+            plt.plot([-1, -0.5, 0, 0.5, 1], [-1.0 * self.anharmonic_forces.flatten().std() for i in range(5)], 'k--')
+
         plt.tick_params(axis='both', which='major', labelsize=10)
         plt.colorbar().set_label(label='probability density', size=15)
 
         plt.tight_layout()
         plt.savefig('joint_PDF.pdf')
-
 
     def structure_averaged_sigma_trajectory(self):
         atoms = [a.label for a in self.ref_frame.asymmetric_unit[0].atoms]
@@ -267,46 +286,22 @@ class AnharmonicScore(object):
                 _sigma_frame = anh_f[i, :, :].flatten().std() / dft_f[i, :, :].flatten().std()
                 self.sigma_frames.append(_sigma_frame)
 
-    def structural_sigma(self):
-        self.__structural_sigma_from_std()
-
-    def __structural_sigma_from_std(self):
+    def structural_sigma(self, return_trajectory=False):
         if self.atom_masks is None:
-            rmse = self.anharmonic_forces.std()
-            std = self.dft_forces.std()
+            rmse = self.anharmonic_forces
+            std = self.dft_forces
         else:
-            rmse = self.anharmonic_forces[:, self.atom_masks, :].std()
-            std = self.dft_forces[:, self.atom_masks, :].std()
-        self.sigma = rmse / std
+            rmse = self.anharmonic_forces[:, self.atom_masks, :]
+            std = self.dft_forces[:, self.atom_masks, :]
 
-    # def __structural_sigma_via_KDE(self):
-    #     import warnings
-    #     warnings.warn("This is only an experimental feature, doesn't seems to be working well and it is very slow!")
-    #     # First get the kernel density estimation for the joint probability distribution between total
-    #     # and the anharmonic forces
-    #     import time
-    #     X = self.trajectory_normalized_dft_forces(flat=True)
-    #     Y = self.trajectory_normalized_anharmonic_forces(flat=True)
-    #
-    #     print("Firstly, performing a non-weighted KDE on the join probability distribution between F and F_anh")
-    #     start_time = time.time()
-    #     values = np.vstack([X, Y])
-    #     nw_kernel = gaussian_kde(values)
-    #
-    #     min_X = min(X)
-    #     min_Y = min(Y)
-    #     max_X = max(X)
-    #     max_Y = max(Y)
-    #
-    #     nw_integrated = nw_kernel.integrate_box([min_X, min_Y], [max_X, max_Y])
-    #     print(nw_integrated, 'time taken: ', time.time() - start_time)
-    #
-    #     X_pred, Y_pred = np.mgrid[-2:2:0.1, -2:2:0.1]
-    #     positions = np.vstack([X_pred.ravel(), Y_pred.ravel()])
-    #     start_time = time.time()
-    #     y_expected = np.sum(np.array([[_y**2*nw_kernel.pdf(np.array([_x,_y]))[0]*0.1**2 for _x in np.arange(-2,2,0.1)] for _y in np.arange(-2,2,0.1)]))
-    #     self.sigma = y_expected
-    #     print('Sigma= '+str(self.sigma)+'  time taken to evaluate PDF on grid ', time.time() - start_time)
+        if not return_trajectory:
+            sigma = rmse.std() / std.std()
+            print("Sigma for entire structure over the MD trajectory is ", str(sigma))
+            return sigma, self.time_series
+        else:
+            print(np.shape(rmse.std(axis=(1, 2), dtype=np.float64)))
+            sigma = rmse.std(axis=(1, 2), dtype=np.float64) / std.std(axis=(1, 2), dtype=np.float64)
+            return sigma, self.time_series
 
 
 if __name__ == "__main__":
@@ -320,9 +315,25 @@ if __name__ == "__main__":
                         help="plot the joint distributions of normalized total and anharmonic forces")
     parser.add_argument("--atom_joint_pdf", action='store_true',
                         help="plot the joint distributions of normalized total and anharmonic forces for each atom in the structure")
+    parser.add_argument("--md_time_step", type=float, default=1,
+                        help="Time step for the molecular dynamic trajectory (in fs), default: 1fs")
+
+    parser.add_argument('--sigma', action='store_true',
+                        help="Return the structural sigma value from this MD trajectory")
+    parser.add_argument('--trajectory', action='store_true',
+                        help="Whether to return sigma for each frame of the MD trajectory")
+
+    parser.add_argument('--plot_trajectory', action='store_true',
+                        help="Whether to plot sigma for each frame of the MD trajectory")
+
+    parser.add_argument("-X", "--X", type=str, default='DFT',
+                        help='data to plot along the X-axis for the joint probability distribution, default: DFT force')
+    parser.add_argument("-Y", "--Y", type=str, default='anh',
+                        help='data to plot along the Y-axis for the joint probability distribution, default: anharmonic forces')
+
     args = parser.parse_args()
 
-    scorer = AnharmonicScore(md_fromes=args.md_xml, ref_frame=args.ref_frame, atoms=None)
+    scorer = AnharmonicScore(md_frames=args.md_xml, ref_frame=args.ref_frame, atoms=None, potim=args.md_time_step)
 
     from matplotlib import rc
 
@@ -330,7 +341,16 @@ if __name__ == "__main__":
     rc('text', usetex=True)
 
     if args.joint_pdf:
-        scorer.plot_total_joint_distribution()
+        scorer.plot_total_joint_distribution(x=args.X, y=args.Y)
 
     if args.atom_joint_pdf:
         scorer.plot_atom_joint_distributions()
+
+    if args.sigma:
+        sigma, time_stps = scorer.structural_sigma(return_trajectory=args.trajectory)
+        if args.plot_trajectory:
+            plt.plot(time_stps,sigma,'b-')
+            plt.xlabel("Time (fs)",fontsize=16)
+            plt.ylabel("$\\sigma(t)$",fontsize=16)
+            plt.tight_layout()
+            plt.savefig("sigma_trajectory.pdf")

doping/energy_landscape.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import sqlite3
 import json
+import math
+import pickle
 
 from ase.db import connect
 from itertools import permutations
@@ -16,12 +18,12 @@ rc('text', usetex=True)
 
 import matplotlib.pylab as pylab
 
-params = {'legend.fontsize': '16',
-          'figure.figsize': (7, 6),
-          'axes.labelsize': 28,
+params = {'legend.fontsize': '10',
+          'figure.figsize': (5, 4),
+          'axes.labelsize': 17,
           'axes.titlesize': 28,
-          'xtick.labelsize': 20,
-          'ytick.labelsize': 20}
+          'xtick.labelsize': 12,
+          'ytick.labelsize': 12}
 pylab.rcParams.update(params)
 
 from core.internal.builders.crystal import map_ase_atoms_to_crystal
@@ -48,7 +50,7 @@ def plot_all_reactions_for_one_system(db, output=None, all_keys=None, X='Cl3'):
              composition_dependent_binary_oxidation_energies,
              composition_dependent_sn_only_oxidation_energies,
              composition_dependent_sn_pb_rhm_oxidiation_energies]
-             #composition_dependent_snpb_rhm_solution_oxidation_energies]
+    # composition_dependent_snpb_rhm_solution_oxidation_energies]
 
     reaction_label_dict = {0: "$\\Delta H_{\\mbox{demix}}$ ",
                            1: "$\\Delta H_{\\mbox{binary-decomp}}$ ",
@@ -160,7 +162,7 @@ def plot_all_reaction_energies_for_system(db, reaction='demixing', output=None, 
         max_energies = [n for _, n in sorted(zip(av_compositions, max_energies))]
         min_energies = [n for _, n in sorted(zip(av_compositions, min_energies))]
         averaged_energies = [n for _, n in sorted(zip(av_compositions, averaged_energies))]
-        #if reaction == 'demixing':
+        # if reaction == 'demixing':
         #    popt, pcov = curve_fit(bowing_curve, x, max_energies)
         #    max_energies = bowing_curve(np.array(x), *popt)
         # else:
@@ -171,8 +173,8 @@ def plot_all_reaction_energies_for_system(db, reaction='demixing', output=None, 
         plt.plot(x, max_energies, '--', c=color_dictionary[counter])
 
         # if reaction == 'demixing':
-        #popt, pcov = curve_fit(bowing_curve, x, min_energies)
-        #min_energies = bowing_curve(np.array(x), *popt)
+        # popt, pcov = curve_fit(bowing_curve, x, min_energies)
+        # min_energies = bowing_curve(np.array(x), *popt)
         # else:
         #    _mine = min_energies
         #    slope, intercept, r_value, p_value, std_err = stats.linregress([x[0],x[-1]], [_mine[0], _mine[-1]])
@@ -183,8 +185,8 @@ def plot_all_reaction_energies_for_system(db, reaction='demixing', output=None, 
         plt.fill_between(x, min_energies, max_energies, color=color_dictionary[counter], alpha=.25)
 
         # if reaction == 'demixing':
-        #popt, pcov = curve_fit(bowing_curve, x, averaged_energies)
-        #averaged_energies = bowing_curve(np.array(x), *popt)
+        # popt, pcov = curve_fit(bowing_curve, x, averaged_energies)
+        # averaged_energies = bowing_curve(np.array(x), *popt)
         # else:
         #    _ave = averaged_energies
         #    slope, intercept, r_value, p_value, std_err = stats.linregress([x[0],x[-1]], [_ave[0],_ave[-1]])
@@ -193,7 +195,7 @@ def plot_all_reaction_energies_for_system(db, reaction='demixing', output=None, 
 
         plt.plot(x, averaged_energies, 'o-', c=color_dictionary[counter], lw=2.5,
                  label=label_dict[counter])
-        #plt.plot(x,[0 for _ in x],'k:')
+        # plt.plot(x,[0 for _ in x],'k:')
 
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
 
@@ -675,13 +677,16 @@ def composition_dependent_binary_halide_decomposition_energies(a, b, c, all_keys
     return reaction_energies
 
 
-def composition_dependent_demixing_energies(a, b, c, all_keys, db):
+def composition_dependent_demixing_energies(a, b, c, all_keys, db, demixing=True):
     end_members = [_a + _b + _c for _a in a for _b in b for _c in c]
     print(end_members)
     assert (len(end_members) == 2)
     mixed_site = [site for site in [a, b, c] if len(site) == 2][-1]
     mixed_site = [''.join([_i for _i in k if not _i.isdigit()]) for k in mixed_site]
     end_member_total_energies = {k: 0 for k in mixed_site}
+
+    mixing_energies = {}
+
     # get the total energies of the two end members
     for m in mixed_site:
         for em in end_members:
@@ -690,11 +695,20 @@ def composition_dependent_demixing_energies(a, b, c, all_keys, db):
                 row = db.get(selection=[('uid', '=', matched_key)])
                 total_energy = row.key_value_pairs['total_energy']
                 end_member_total_energies[m] = total_energy
+
+                structure = map_ase_atoms_to_crystal(row.toatoms())
+                element_1 = ''.join([_i for _i in mixed_site[0] if not _i.isdigit()])
+
+                if element_1 in structure.all_atoms_count_dictionaries().keys():
+                    composition = 1.0
+                else:
+                    composition = 0.0
+
                 print(m, total_energy)
+                if not demixing:
+                    mixing_energies[composition] = [total_energy / structure.total_num_atoms()]
     # figure out which site has been mixed with two chemical elements, then we can decide
     #   the chemical compositions should be measured against which element
-
-    mixing_energies = {}
 
     system_content = a + b + c
     for k in all_keys:
@@ -706,6 +720,7 @@ def composition_dependent_demixing_energies(a, b, c, all_keys, db):
             structure = map_ase_atoms_to_crystal(row.toatoms())
             element_1 = ''.join([_i for _i in mixed_site[0] if not _i.isdigit()])
             element_2 = ''.join([_i for _i in mixed_site[1] if not _i.isdigit()])
+            print(element_1)
             composition = structure.all_atoms_count_dictionaries()[element_1] / (
                     structure.all_atoms_count_dictionaries()[element_1] + structure.all_atoms_count_dictionaries()[
                 element_2])
@@ -719,9 +734,138 @@ def composition_dependent_demixing_energies(a, b, c, all_keys, db):
 
             if composition not in mixing_energies.keys():
                 mixing_energies[composition] = []
-
-            mixing_energies[composition].append(mixing_energy)
+            if demixing:
+                mixing_energies[composition].append(mixing_energy)
+            else:
+                mixing_energies[composition].append(total_energy / structure.total_num_atoms())
     return mixing_energies
+
+
+def demixing_free_energies_with_configurational_entropy(a, b, c, all_keys, db, temperature, total_energy_dict):
+    demixing_free_energy_dict = {}
+    kb = 8.617e-5  # eV/K
+
+    comp = 0.0
+    free_en_mix_0 = [math.exp(-1.0 * e / (kb * temperature)) for e in total_energy_dict[comp]]
+    free_en_mix_0 = -kb * temperature * math.log(sum(free_en_mix_0))
+
+    comp = 1.0
+    free_en_mix_1 = [math.exp(-1.0 * e / (kb * temperature)) for e in total_energy_dict[comp]]
+    free_en_mix_1 = -kb * temperature * math.log(sum(free_en_mix_1))
+
+    for comp in list(sorted(total_energy_dict.keys())):
+        free_en_mix = [math.exp(-1.0 * e / (kb * temperature)) for e in total_energy_dict[comp]]
+        l = len(free_en_mix)
+        free_en_mix = -kb * temperature * math.log(sum(free_en_mix))
+        free_en_mix = free_en_mix - ((1.0 - comp) * free_en_mix_0 + comp * free_en_mix_1)
+        demixing_free_energy_dict[comp] = free_en_mix
+        # print(comp, l, free_en_mix)
+    # print('/n')
+    return demixing_free_energy_dict
+
+
+def get_helmhotz_fe(c, comp):
+    # This is hard coded for the time-being
+    helmholtz_data = pickle.load(open('/scratch/dy3/jy8620/Sn_halide_perovskite/equ_MD/free_energies.bp', 'rb'))
+    for k in helmholtz_data.keys():
+        if (c[-1] == helmholtz_data[k]['X']):
+            if (comp == helmholtz_data[k]['composition']):
+                return helmholtz_data[k]['free_energy']
+
+
+def demixing_free_energies_with_configurational_entropy_from_helmholtz(a, b, c, all_keys, db, temperature,
+                                                                       total_energy_dict):
+    demixing_free_energy_dict = {}
+    kb = 8.617e-5  # eV/K
+
+    comp = 0.0
+    hel_fe = get_helmhotz_fe(c, comp)
+    energies = [e + hel_fe[temperature] * 1.0362E-2 for e in total_energy_dict[comp]]
+    free_en_mix_0 = [math.exp(-1.0 * e / (kb * temperature)) for e in energies]
+    free_en_mix_0 = -kb * temperature * math.log(sum(free_en_mix_0))
+
+    comp = 1.0
+    hel_fe = get_helmhotz_fe(c, comp)
+    energies = [e + hel_fe[temperature] * 1.0362E-2 for e in total_energy_dict[comp]]
+    free_en_mix_1 = [math.exp(-1.0 * e / (kb * temperature)) for e in energies]
+    free_en_mix_1 = -kb * temperature * math.log(sum(free_en_mix_1))
+
+    for comp in list(sorted(total_energy_dict.keys())):
+        hel_fe = get_helmhotz_fe(c, comp)
+        energies = [e + hel_fe[temperature] * 1.0362E-2 for e in total_energy_dict[comp]]
+        free_en_mix = [math.exp(-1.0 * e / (kb * temperature)) for e in energies]
+        free_en_mix = -kb * temperature * math.log(sum(free_en_mix))
+        free_en_mix = free_en_mix - ((1.0 - comp) * free_en_mix_0 + comp * free_en_mix_1)
+        demixing_free_energy_dict[comp] = free_en_mix
+    return demixing_free_energy_dict
+
+
+def plot_demixing_free_energies_with_configurational_entropy(db, a=None, b=None, c=None, output=None, all_keys=None,
+                                                             phonon=False):
+    from cycler import cycler
+    from scipy.signal import savgol_filter
+    from scipy import interpolate
+
+    color = plt.cm.Blues(np.linspace(0, 1, 8))
+    pylab.rcParams['axes.prop_cycle'] = cycler('color', color)
+    total_energy_dict = composition_dependent_demixing_energies(a, b, c, all_keys, db, demixing=False)
+    for temp in [100, 200, 300, 400, 500, 600, 700, 800]:
+        demixing_free_energy_dict = demixing_free_energies_with_configurational_entropy_from_helmholtz(a, b, c,
+                                                                                                       all_keys, db,
+                                                                                                       temp,
+                                                                                                       total_energy_dict)
+        compositions = list(sorted(demixing_free_energy_dict.keys()))
+        plt.plot(compositions, [demixing_free_energy_dict[k] for k in compositions], 'o-',
+                 label=str(temp) + ' K')
+
+    demixing_free_energy_dict = demixing_free_energies_with_configurational_entropy_from_helmholtz(a, b, c,
+                                                                                                   all_keys, db,
+                                                                                                   800,
+                                                                                                   total_energy_dict)
+    plt.plot(compositions, [demixing_free_energy_dict[k] for k in compositions], 'o-',
+             label='With $F_{vib}$',c=color[-1])
+
+    new_color = plt.cm.Blues(np.linspace(0, 1, 8))
+    pylab.rcParams['axes.prop_cycle'] = cycler('color', new_color)
+    total_energy_dict = composition_dependent_demixing_energies(a, b, c, all_keys, db, demixing=False)
+    for temp in [100, 200, 300, 400, 500, 600, 700, 800]:
+        demixing_free_energy_dict = demixing_free_energies_with_configurational_entropy(a, b, c, all_keys, db,
+                                                                                        temp, total_energy_dict)
+
+        compositions = list(sorted(demixing_free_energy_dict.keys()))
+        plt.plot(compositions, [demixing_free_energy_dict[k] for k in compositions], '--')
+
+    demixing_free_energy_dict = demixing_free_energies_with_configurational_entropy(a, b, c,
+                                                                                    all_keys, db,
+                                                                                    800,
+                                                                                    total_energy_dict)
+    plt.plot(compositions, [demixing_free_energy_dict[k] for k in compositions], '--',
+             label='Without $F_{vib}$',c=new_color[-1])
+
+    x_label = '$x$ in '
+    if len(a) == 1:
+        x_label += fix_string(a[0])
+    else:
+        raise NotImplementedError()
+    if len(b) == 1:
+        x_label += fix_string(b[0])
+    else:
+        mixed_site = [''.join([_i for _i in k if not _i.isdigit()]) for k in b]
+        x_label += '('
+        x_label += str(mixed_site[0]) + '$_{x}$'
+        x_label += str(mixed_site[1]) + '$_{1-x}$'
+        x_label += ')'
+    if len(c) == 1:
+        x_label += fix_string(c[0])+'$_{3}$'
+    else:
+        raise NotImplementedError()
+
+    plt.xlabel(x_label)
+    plt.ylabel('$\Delta G_{mix}(x)$ (eV/atom)')
+    plt.ylim([-0.3,0.025])
+    plt.legend(loc=4)
+    plt.tight_layout()
+    plt.savefig(output)
 
 
 if __name__ == "__main__":
@@ -748,6 +892,10 @@ if __name__ == "__main__":
     parser.add_argument("--reaction", type=str, default=None,
                         help='Plot reaction energy landscape across all compositions')
     parser.add_argument("--summary", action='store_true', help="Plot a summary for multiple systems")
+
+    parser.add_argument("--free_energies", action='store_true', help="Plot demixing free energies")
+    parser.add_argument("--phonon", action='store_true',
+                        help='whether vibrational contribution to free energies should be included.')
     args = parser.parse_args()
 
     if os.path.exists(args.db):
@@ -778,3 +926,9 @@ if __name__ == "__main__":
                                                  all_keys=all_uids)
     if args.reaction:
         plot_all_reaction_energies_for_system(db, args.reaction, args.output, all_keys=all_uids)
+
+    if args.free_energies:
+        plot_demixing_free_energies_with_configurational_entropy(db, a=args.a_site, b=args.b_site, c=args.c_site,
+                                                                 output=args.output,
+                                                                 all_keys=all_uids,
+                                                                 phonon=args.phonon)